fd35ff2b9ce73300a2f5310783fef586f6a90818
nave <neelakandan@cs.wisc.edu>
2015-05-04 03:40:02 -0500

    add PolicySetter pq

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/mover/PolicySetter.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/mover/PolicySetter.java
@@ -4,9 +4,10 @@ import java.util.PriorityQueue;
 
 public class PolicySetter{
 
+	PriorityQueue<Object> priorityQueue;	
 	private static final int WIDTH = 10;
 
-	public void setPolicy(PriorityQueue<Object> priorityQueue){
+	public void setPolicy(){
 		Object priorityFile = null;
 		while((priorityFile = priorityQueue.poll()) != null){
 				 
